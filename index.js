798e909ddcbd57c43af079cd94e1a3a39068cf94
SilverMaiden <h.specials@gmail.com>
2020-01-13 14:21:32 -0800

    Completed parts of find and findById

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // implement your API here
+//
+
+const express = require('express');
+const port = 5000;
+
+const server = express();
+
+const data = require('./data/db');
+
+
+  server.get('/users', (req, res) => {
+    data.find()
+    .then(data => {
+        res.status(200).json(data)
+    })
+    .catch(error => {
+        console.log(error);
+    })
+})
+
+  server.get('/users/:id', (req, res) => {
+      data.findById(req.params.id)
+      .then(data => {
+          res.status(200).json(data)
+      })
+      .catch(error => {
+          console.log(error);
+      })
+  })
+
+server.listen(port, () => {
+    console.log(`server listening on port ${port}`);
+})
